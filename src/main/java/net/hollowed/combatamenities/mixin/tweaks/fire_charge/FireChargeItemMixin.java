a545c67357790349703a059b509ae3a7cad3d858
HollowedWanderer <122174504+HollowedWanderer@users.noreply.github.com>
2025-07-25 21:55:31 -0400

    FIREBALL!!!

--- src/main/java/net/hollowed/combatamenities/mixin/tweaks/fire_charge/FireChargeItemMixin.java
+++ src/main/java/net/hollowed/combatamenities/mixin/tweaks/fire_charge/FireChargeItemMixin.java
@@ -1,4 +1,4 @@
-package net.hollowed.combatamenities.mixin.tweaks.fireCharge;
+package net.hollowed.combatamenities.mixin.tweaks.fire_charge;
 
 import net.hollowed.combatamenities.config.CAConfig;
 import net.minecraft.entity.player.PlayerEntity;
@@ -6,13 +6,15 @@ import net.minecraft.entity.projectile.SmallFireballEntity;
 import net.minecraft.item.FireChargeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FireChargeItem.class)
 public abstract class FireChargeItemMixin extends Item {
@@ -24,26 +26,23 @@ public abstract class FireChargeItemMixin extends Item {
     @Unique
     public ActionResult use(World world, PlayerEntity player, Hand hand) {
         if (!world.isClient) {
-
             if (CAConfig.throwableFirecharge) {
-                // Get the ItemStack in hand
+                world.syncWorldEvent(null, 1018, player.getBlockPos(), 0);
                 ItemStack stack = player.getStackInHand(hand);
                 player.swingHand(hand, true);
-
-                SmallFireballEntity fireball = new SmallFireballEntity(player.getWorld(), player.getX(), player.getY() + 1.5, player.getZ(), player.getRotationVec(0));
-
-                // Spawn the fireball entity
+                SmallFireballEntity fireball = new SmallFireballEntity(player.getWorld(), player, player.getRotationVec(1.0F).normalize().multiply(2));
+                fireball.setPosition(fireball.getX(), player.getEyeY(), fireball.getZ());
                 world.spawnEntity(fireball);
-
-                // Consume one fire charge
                 stack.decrementUnlessCreative(1, player);
-                world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.NEUTRAL, 0.5F, 1F);
-                player.getItemCooldownManager().set(stack, 10);
-
-                // Indicate successful use
+                player.getItemCooldownManager().set(stack, 6);
                 return ActionResult.SUCCESS;
             }
         }
         return ActionResult.PASS;
+    }
+
+    @Inject(method = "useOnBlock", at = @At("HEAD"), cancellable = true)
+    private void useOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+        cir.setReturnValue(ActionResult.PASS);
     }
 }
